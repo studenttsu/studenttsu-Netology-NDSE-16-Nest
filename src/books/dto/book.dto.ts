1411b7dd1730d6f63cc5334df4d473fe775d28dd
Konstantin Teryohin <konstantin.teryohin@rubius.com>
2023-03-21 02:36:50 +0700

    rxjs

--- src/books/dto/book.dto.ts
+++
@@ -1,8 +0,0 @@
-export class BookDto {
-    title: string;
-    description: string;
-    authors: string;
-    fileCover: string;
-    fileName: string;
-    fileBook: string;
-}
--- src/books/dto/create-update-book.dto.ts
+++
@@ -1,8 +0,0 @@
-export class CreateUpdateBookDto {
-    title: string;
-    description: string;
-    authors: string;
-    fileCover: string;
-    fileName: string;
-    fileBook: string;
-}
