f307192686eff5b140de03c195023784524e46ca
Konstantin Teryohin <konstantin.teryohin@rubius.com>
2023-03-21 03:22:06 +0700

    class-validator

--- src/books/dto/create-update-book.dto.ts
+++ src/books/dto/create-update-book.dto.ts
@@ -1,8 +1,24 @@
+import { IsEmpty, IsString } from 'class-validator';
+
 export class CreateUpdateBookDto {
+  @IsString()
   title: string;
+
+  @IsString()
   description: string;
+
+  @IsString()
   authors: string;
+
+  @IsString()
+  @IsEmpty()
   fileCover: string;
+
+  @IsString()
+  @IsEmpty()
   fileName: string;
+
+  @IsString()
+  @IsEmpty()
   fileBook: string;
 }
